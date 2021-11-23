a42e476dde32a5bda1ace3847930b5642ef10b46
andrey-khristofor <72157417+andrey-khristofor@users.noreply.github.com>
2021-11-23 10:43:59 +0200

    Add files via upload

---
+++ Lab3.1/src/App.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.regex.*;
+import java.util.*;
+
+public class App {
+    public static enum LexemeType {
+        Whitespace("[\t\n\r ]+"),
+        Number("\\b\\d+(\\.\\d+)?(e[+-]?\\d+)?\\b"),
+        String("\"[^\"]*\"|'[^']*'|`[^`]*`"),
+        Comment("//.*"),
+        Keyword("\\b(break|case|catch|class|const|continue|debugger|default|delete|do|else|export|extends|finally|for|function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|while|with|yield)\\b"),
+        Identifier("\\b[a-zA-Z_$][a-zA-Z_$0-9]*\\b"),
+        Operator("(\\+\\+|\\+|--|-|\\*\\*|/|%|\\*|<<|<|>>>|>>|>|==|=|!=|!|&&|\\^|$\\|\\||&|\\||\\?\\?)=?|\\?|~"),
+        Punctuation("[;:,\\.\\{\\}\\[\\]\\(\\)]");
+
+        public final String pattern;
+
+        private LexemeType(String pattern) {
+            this.pattern = pattern;
+        }
+    };
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter filename: ");
+        String filename = reader.next();
+        reader.close();
+        if (parseCode(filename)) {
+            System.out.println("\nParsed");
+        } else {
+            System.out.println("\nParsing failed");
+        }
+    }
+
+    public static boolean parseCode(String filename) {
+        String source;
+        try {
+            source = Files.readString(Path.of(filename));
+        } catch (IOException e) {
+            System.err.println(e);
+            return false;
+        }
+
+        StringBuffer patternsBuffer = new StringBuffer();
+        for (LexemeType type : LexemeType.values()) {
+            patternsBuffer.append(String.format("|(%s)", type.pattern));
+        }
+
+        Pattern pattern = Pattern.compile(new String(patternsBuffer.substring(1)));
+        Matcher matcher = pattern.matcher(source);
+
+        int start = 0;
+        while (matcher.find()) {
+            if (start != matcher.start()) {
+                 System.err.println("\nInvalid token: " + source.substring(start, matcher.start()));
+                 return false;
+            } else {
+                start = matcher.end();
+            }
+            for (LexemeType type: LexemeType.values()) {
+                if (matcher.group().matches(type.pattern)) {
+                    if (type != LexemeType.Whitespace && type != LexemeType.Comment) {
+                        System.out.println(matcher.group() + " - " + type.name());
+                    }
+                    if (type == LexemeType.Comment) {
+                        System.out.println(type.name());
+                    }
+                    break;
+                }
+            }
+        }
+
+        return true;
+    }
+}
